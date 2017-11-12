04e843deaa0c1e396345cec728e35c4c258a473f
Lazar Zamurovic <zamurovicl@carleton.edu>
2017-11-12 17:27:20 -0600

    Stop words and punctuation removed

--- TextSentimentAnalysis.py
+++ TextSentimentAnalysis.py
@@ -1,18 +1,23 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.feature_extraction import text
+import string
+import re
+
+stop_words = text.ENGLISH_STOP_WORDS
 
 sentimentTags = pd.read_csv('Sentiment_Tags.csv')
-solomonTexts = pd.read_excel('SolomonTexts.xls')
+#solomonTexts = pd.read_excel('SolomonTexts.xls')
+#
+#solomonMegTexts = solomonTexts[solomonTexts['Address'] == '+16085153471']['Body'].tolist()
+#solomonMegTexts.reverse()
 
-solomonMegTexts = solomonTexts[solomonTexts['Address'] == '+16085153471']['Body'].tolist()
-solomonMegTexts.reverse()
-
-annaliseTexts = open('Annalise_Annalise.txt', 'r').readlines()
+annaliseTexts = open('Annalise_Annalise.txt', 'r', encoding = "utf-8").readlines()
 annaliseTexts = list(map(lambda x: x.lower().strip("\n"), annaliseTexts))
 
-ryanTexts = open('Ryan_Ryan.txt', 'r').readlines()
-ryanTexts = list(map(lambda x: x.lower().strip("\n"), ryanTexts))
+#ryanTexts = open('Ryan_Ryan.txt', 'r', encoding = 'utf-8').readlines()
+#ryanTexts = list(map(lambda x: x.lower().strip("\n"), ryanTexts))
 
 sentimentWord = sentimentTags['Word'].tolist()
 sentimentScore = sentimentTags['Score'].tolist()
@@ -20,29 +25,41 @@ sentimentDict = {}
 for i in range(len(sentimentWord)):
     sentimentDict[sentimentWord[i]] = sentimentScore[i]
 
-print(sentimentDict)
-ax = plt.axes()
-ax.set_title('Distribution of Scores for Tagged Words')
-ax.set_ylabel('Score')
-ax.set_xlabel('Words (Ordered)')
-plt.scatter(list(range(len(sentimentDict.values()))), sentimentDict.values(), color = 'blue', marker = 'x', s = 15, label = 'score')
-plt.show()
+#ax = plt.axes()
+#ax.set_title('Distribution of Scores for Tagged Words')
+#ax.set_ylabel('Score')
+#ax.set_xlabel('Words (Ordered)')
+#plt.scatter(list(range(len(sentimentDict.values()))), sentimentDict.values(), color = 'blue', marker = 'x', s = 15, label = 'score')
+#plt.show()
 
 def computeSentenceAverages(inputText):
     cumulativeAverageScore, sentenceAverages = 0, []
     for i in range(len(inputText)):
         wordlist = inputText[i].lower().split(" ")
         cumulativeScore = 0
+        stopWordPunctScore = 0
         for word in wordlist:
-            word = word.strip('.').strip(',')
-            try:
-                score = sentimentDict[word]
-            except KeyError:
-                score = 4.5
+            word =  "".join(l for l in word if l not in string.punctuation)
+            if word in stop_words or word in string.punctuation:
+                stopWordPunctScore = stopWordPunctScore + 1
+                continue
+            else:
+                try:
+                    score = sentimentDict[word]
+                except KeyError:
+                    score = 4.5
 
             cumulativeScore += score
 
-        sentenceAverageScore = cumulativeScore / len(wordlist)
+        sentLenStopWord = len(wordlist) - stopWordPunctScore
+        if sentLenStopWord == 0:
+            sentenceAverageScore = 4.5
+        else:
+            sentenceAverageScore = cumulativeScore / sentLenStopWord
+        print("Score:", cumulativeScore)
+        print("Length:", len(wordlist))
+        print("StopWords:", stopWordPunctScore)
+        print("Final:", sentenceAverageScore)
         cumulativeAverageScore += sentenceAverageScore
         sentenceAverages.append(sentenceAverageScore)
 
@@ -69,6 +86,8 @@ def main():
         combinedList.append([annaliseAvgs[i], annaliseText_[i]])
 
     combinedList.sort()
+    
+    print(combinedList)
 
 
 
